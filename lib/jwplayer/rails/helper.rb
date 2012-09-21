c8081c85b7643e394e670f7fe454e7f19edc4150
Anton Volkov <choixer@gmail.com>
2012-09-21 12:56:19 +0400

    Change location of flash player

--- lib/jwplayer/rails/helper.rb
+++ lib/jwplayer/rails/helper.rb
@@ -2,7 +2,7 @@ module JWPlayer::Rails
   module Helper
     DEFAULT_OPTIONS = {
       id: 'jwplayer',
-      flashplayer: '/swf/player.swf',
+      flashplayer: '/assets/player.swf',
       width: '400',
       height: '300'
     }
